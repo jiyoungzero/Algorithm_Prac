96e980de0c4098ea04911409c7fe29e9444ea95a
LeeJY98 <wldudwldhd@naver.com>
2021-12-28 23:58:27 +0900

    ..

--- brute_14502.py
+++ brute_14502.py
@@ -1,6 +1,8 @@
 row, col = tuple(map(int, input().split()))
 max_area = 0
 
+maze = [list(map(int, input().split())) for _ in range(row)]
+
 for i in range(row):
     for j in range(col):
-        
+        if 
