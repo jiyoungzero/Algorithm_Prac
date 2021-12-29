4f12ca07d6ecf27629746cf99a69f6340db7f012
LeeJY98 <wldudwldhd@naver.com>
2021-12-29 22:54:52 +0900

    ..

--- brute_14502.py
+++ brute_14502.py
@@ -5,5 +5,5 @@ maze = [list(map(int, input().split())) for _ in range(row)]
 
 for i in range(row):
     for j in range(col):
-        if maze[i][j] == '0':
+        if maze[i][j] == '0' and maze[i+1][j] == '2':
             
