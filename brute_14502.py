74add86f0246b1a652f287fb57bf1de93bb08ed9
LeeJY98 <wldudwldhd@naver.com>
2021-12-30 17:56:04 +0900

    map thinking2

--- brute_14502.py
+++ brute_14502.py
@@ -1,9 +1,17 @@
 row, col = tuple(map(int, input().split()))
 max_area = 0
-dx = [0, 0, 1, -1]
-dy = [-1, 1,0, 0]
+dxs = [0, 0, 1, -1]
+dys = [-1, 1,0, 0]
 maze = [list(map(int, input().split())) for _ in range(row)]
 
-for i in range(row):
-    for j in range(col):
-'
+def in_range(x, y): 
+    return 0 <= x and x < col and 0 <= y and y < row
+
+for x in range(row):
+    for y in range(col):
+        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and arr[nx][ny] == 1:
+                cnt += 1    
+        if cnt >= 3:
