71a73b14a87336353d6947791ab4c31a72953eeb
LeeJY98 <wldudwldhd@naver.com>
2021-12-30 17:47:59 +0900

    map thinking

--- brute_14502.py
+++ brute_14502.py
@@ -1,9 +1,9 @@
 row, col = tuple(map(int, input().split()))
 max_area = 0
-
+dx = [0, 0, 1, -1]
+dy = [-1, 1,0, 0]
 maze = [list(map(int, input().split())) for _ in range(row)]
 
 for i in range(row):
     for j in range(col):
-        if maze[i][j] == '0' and maze[i+1][j] == '2':
-            
+'
