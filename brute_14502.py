d4ebfa32fd76398d9597434ea5ef005d44e3d9d2
LeeJY98 <wldudwldhd@naver.com>
2021-12-31 23:11:54 +0900

    map thinking3

--- brute_14502.py
+++ brute_14502.py
@@ -3,15 +3,23 @@ max_area = 0
 dxs = [0, 0, 1, -1]
 dys = [-1, 1,0, 0]
 maze = [list(map(int, input().split())) for _ in range(row)]
+cnt = 0
 
+def wall(w_cnt):
+    if w_cnt == 3:
+        dfs()
+    else:
+        w_cnt+=1
+        wall(w_cnt)
+        dfs()
+        
 def in_range(x, y): 
     return 0 <= x and x < col and 0 <= y and y < row
 
-for x in range(row):
-    for y in range(col):
-        dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if in_range(nx, ny) and arr[nx][ny] == 1:
-                cnt += 1    
-        if cnt >= 3:
+def dfs():
+    for x in range(row):
+        for y in range(col):
+            for dx, dy in zip(dxs, dys):
+                nx, ny = x + dx, y + dy
+                if in_range(nx, ny) and maze[x][y] == 0 and maze[nx][ny] == 2:
+                    maze[x][y] == 2
