788bd3c73958fc3532aaf2b4eb9c411ad786dcc1
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-13 23:56:15 +0900

    bfs

--- dfs&bfs_1260.py
+++ dfs&bfs_1260.py
@@ -1,3 +1,4 @@
+# node, branch, first node
 n, m, v = tuple(map(int, input().split()))
 
 # depth first search
@@ -8,4 +9,15 @@ def dfs(n):
         if not visited[i]:
             dfs(i) # 깊이탐색은 재귀로 해결
 
-            
+# breadth first search
+def bfs(n):
+    visited[n] = True
+    queue = deque([n])
+    while queue:
+        v = queue.popleft()
+        print(v, end =' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visitied[i] = True #너비탐색은 큐에 저장 후 반복하며 탐색
+
