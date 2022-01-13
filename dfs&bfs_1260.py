f7bc361e414f97fea36c988b00cd504db2d5f133
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-13 23:52:46 +0900

    dfs

--- dfs&bfs_1260.py
+++ dfs&bfs_1260.py
@@ -1,4 +1,11 @@
 n, m, v = tuple(map(int, input().split()))
 
-for _ in range(m):
-    dfs
+# depth first search
+def dfs(n):
+    print(n, end=' ')
+    visited[n] = True
+    for i in graph[n]:
+        if not visited[i]:
+            dfs(i) # 깊이탐색은 재귀로 해결
+
+            
