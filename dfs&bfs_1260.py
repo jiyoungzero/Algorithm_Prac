ec8534b0bfbca5909110def289124119c0a159da
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-15 16:18:10 +0900

    2commit : bfs, dfs

--- dfs&bfs_1260.py
+++ dfs&bfs_1260.py
@@ -1,24 +1,37 @@
+from collections import deque
+
 # node, branch, first node
 n, m, v = tuple(map(int, input().split()))
 
 # depth first search
 def dfs(n):
+    #현재 노드를 방문 처리
     print(n, end=' ')
     visited[n] = True
+
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[n]:
         if not visited[i]:
-            dfs(i) # 깊이탐색은 재귀로 해결
+            dfs(i) 
 
 # breadth first search
 def bfs(n):
-    visited[n] = True
+    #큐 구현을 이해 deque 라이브러리 이용
     queue = deque([n])
+    visited[n] = True    
+
+    # 큐가 빌 때까지 반복
     while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
         v = queue.popleft()
         print(v, end =' ')
+        # 아직 방문하지 않은 인접한 원소들을 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
-                visitied[i] = True #너비탐색은 큐에 저장 후 반복하며 탐색
+                visitied[i] = True 
 
-ss
+graph = [[] * (n+1)]
+visited = [False] * (n+1)
+
+
