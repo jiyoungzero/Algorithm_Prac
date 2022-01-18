3b804349f4ff79a6557be0b5bb8f14d67ed992d8
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-18 22:54:53 +0900

    faster by using only 2 for

--- farm,boj_triangle.py
+++ farm,boj_triangle.py
@@ -1,11 +1,12 @@
 n = int(input())
 cnt = 0
-
+long = 0
 for i in range(1, n):
-    for j in range(1, n):
-        for k in range(1, n):
-            if i<=j and j<=k and k<i+j and (i+j+k) == n:
-                cnt+=1
-            else:
-                continue
+	for j in range(i, n):
+		k = n-(i+j)
+		long = max(i, j, k)
+		if long < n-long and j<=k:
+			cnt+=1
+		else:
+			continue
 print(cnt)
