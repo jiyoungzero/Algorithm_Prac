0cd6aee15cd3bc97f7400d5d3583a462d5ae6add
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-28 16:56:34 +0900

    5th retry for faster

--- farm,boj_biggerthanM,14246.py
+++ farm,boj_biggerthanM,14246.py
@@ -1,14 +1,16 @@
 n = int(input())
 arr = list(map(int, input().split()))
 m = int(input())
-s =0 
-cnt = 0
-stop = 0
+s,cnt, stop = 0,0,0
+
 for a in range(n):
-	s= 0
-	while(s <= m):
-		for b in range(a,n):
-			s+=arr[b]
+	s = 0
+	for b in range(stop,n):
+    	# a부터 stop까지 
+		s = sum(arr[a:b+1])
+		if s > m : # m을 초과하는 합이 발생하는 끝지점 stop
 			stop = b
-	cnt += n-stop+1
+			break
+	if s>m:
+		cnt += n-stop # 나머지 원소가 있는 만큼 더하기
 print(cnt)
