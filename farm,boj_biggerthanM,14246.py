9a82572e50a20313f356b534806692df958caa14
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-28 15:17:38 +0900

    try to make faster_timelimit

--- farm,boj_biggerthanM,14246.py
+++ farm,boj_biggerthanM,14246.py
@@ -1,7 +1,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 m = int(input())
-
-for i in range(n):
-	for j in range(i+1,m):
-		print(sum(arr[i:j]))
+s =0 
+cnt = 0
+stop = 0
+for a in range(n):
+	s= 0
+	while(s <= m):
+		for b in range(a,n):
+			s+=arr[b]
+			stop = b
+	cnt += n-stop+1
+print(cnt)
