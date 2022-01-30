84750eb17926f4a92e2cf8828038a6a739722d4f
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-31 01:21:47 +0900

    need to studying code

--- boj,farm_9663,n_queen.py
+++ boj,farm_9663,n_queen.py
@@ -1,18 +1,21 @@
 n = int(input())
 row = [0]*n
 ans = 0
+ver = [False]*(n+1)
+diag1 = [False]*(2*n+1)
+diag2 = [False]*(2*n+1)
 
 def n_queen(x):
-	global ans
-	if x == n:
-		ans += 1
-		return ans
-	else:
-		for i in range(n):
-			# (x,i) / (j, row[j])
-			row[x] = i
-			for j in range(x):
-				if i != row[j] and (x-i) != (j-row[j]) and (x+i) != (j+row[j]):
-					n_queen(x+1)
-n_queen(0)
+    global ans
+    if x == (n+1):
+        ans += 1
+        return ans
+    else:
+        for i in range(1,n+1):
+            if ver[i] or diag1[x+i] or diag2[x -i+n]:continue
+            ver[i] = diag1[x+i] = diag2[x-i+n] = True
+            n_queen(x+1)
+            ver[i] = diag1[x+i] = diag2[x-i+n]=False #백트래킹
+
+n_queen(1)
 print(ans)
