fb8d1f076887c8358099490401f2ff3fefccbf70
Jiyoung Lee <wldudwldhd@naver.com>
2022-01-31 10:54:34 +0900

    final code_succeed

--- boj,farm_9663,n_queen.py
+++ boj,farm_9663,n_queen.py
@@ -1,21 +1,21 @@
 n = int(input())
 row = [0]*n
 ans = 0
-ver = [False]*(n+1)
-diag1 = [False]*(2*n+1)
-diag2 = [False]*(2*n+1)
+ver = [False]*(n) # 열 배열 ver[x] = True 이면 
+diag1 = [False]*(2*n) # / 중 겹치는 게 있는지 
+diag2 = [False]*(2*n) # \ 중 겹치는 게 있는지
 
 def n_queen(x):
     global ans
-    if x == (n+1):
+    if x == (n):
         ans += 1
         return ans
     else:
-        for i in range(1,n+1):
+        for i in range(0,n): # (x, i)
             if ver[i] or diag1[x+i] or diag2[x -i+n]:continue
             ver[i] = diag1[x+i] = diag2[x-i+n] = True
             n_queen(x+1)
             ver[i] = diag1[x+i] = diag2[x-i+n]=False #백트래킹
 
-n_queen(1)
+n_queen(0)
 print(ans)
