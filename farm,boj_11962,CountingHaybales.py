4f51b44ae7bf0e855fc19ef2fda878e5bc5e0885
Jiyoung Lee <wldudwldhd@naver.com>
2022-02-12 02:24:21 +0900

    time limit

--- farm,boj_11962,CountingHaybales.py
+++ farm,boj_11962,CountingHaybales.py
@@ -1,30 +1,30 @@
+import sys
 n, q = tuple(map(int,input().split()))
 pos = list(map(int, input().split()))
+
 def low_search(pos, a):
-	low, high = 0, n
-	while low<high:
-		mid = (low+high)//2
-		if pos[mid] < a:
-			low = mid+1
-		else:
-			high = mid
-	return low
-	
+    low, high = 0, n
+    while low<=high:
+        mid = (low+high)//2
+        if pos[mid] < a:low = mid + 1
+        elif pos[mid] > a : high = mid - 1
+        else: low = pos[mid]
+    return low
+    
 def high_search(pos, a):
-	low, high = 0, n
-	while low<=high:
-		mid = (low+high)//2
-		if pos[mid] <= a:
-			low = mid+1
-		else:
-			high = mid
-	return high
-	
+    low, high = 0, n
+    while low<=high:
+        mid = (low+high)//2
+        if pos[mid] < a:low = mid + 1
+        elif pos[mid] > a : high = mid - 1
+        else: high = pos[mid]
+    return high
+
 for i in range(n):
-	pos.sort() # 오름차순 정렬
+    pos.sort() # 오름차순 정렬
 
 for i in range(q):
-	a, b = tuple(map(int, input().split()))
-	low = low_search(pos, a)
-	high = high_search(pos, b)
-	print(high-low)
+    a, b = tuple(map(int, sys.stdin.readline().split()))
+    low = low_search(pos, a)
+    high = high_search(pos, b)
+    print(high-low)
