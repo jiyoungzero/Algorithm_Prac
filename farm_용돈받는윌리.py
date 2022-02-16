df48967f0814a44f2e92d6d80b64c23fb64df162
Jiyoung Lee <wldudwldhd@naver.com>
2022-02-16 11:55:05 +0900

    make small arr size

--- farm_용돈받는윌리.py
+++ farm_용돈받는윌리.py
@@ -1,8 +1,8 @@
 import sys
 sys.setrecursionlimit(6000000)
 n = int(input())
-dp = [[0 for _ in range(n+2)] for _ in range(n+2)]
-arr = [map(int, sys.stdin.readline().split()) for _ in range(n)]
+dp = [[0]*3 for _ in range(n+2)] # 쓸데없이 배열크기 크게 잡지 마라...!
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 for i in range(1,n+1):
     a, b, c = arr[i-1][0], arr[i-1][1], arr[i-1][2]
