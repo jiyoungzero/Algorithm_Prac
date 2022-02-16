093cb1250b3376c45e118a5b87dc76ef584a75d3
Jiyoung Lee <wldudwldhd@naver.com>
2022-02-17 01:10:51 +0900

    succed_need to retry

--- farm, boj_12865평범한배낭.py
+++ farm, boj_12865평범한배낭.py
@@ -1,13 +1,17 @@
-n, k = tuple(map(int, input().split()))
-arr = [list(map(int, input().split())) for _ in range(n)]
-dp = [[0]*w for _ in range(n)]
+n, k = map(int, input().split())
+arr = [[0,0]]
+d = [[0]*(k+1) for _ in range(n+1)]
 
 for i in range(n):
-    for j in range(k):
+    arr.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
         w = arr[i][0]
         v = arr[i][1]
         if j < w:
-            dp[i][j] = dp[i-1][j]
+            d[i][j] = d[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
-print(dp[n-1][k-1])
+            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v) # i-1 과 같은 것이 있어서 왠만하면 arr를 1부터 시작하도록 [0,0]처음에 추가하기
+
+print(d[n][k])
