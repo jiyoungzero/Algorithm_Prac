c197615459b62e85f5cc329e84e00c3399629626
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-10 23:35:21 +0900

    need to retry & understand

--- boj_동전1.py
+++ boj_동전1.py
@@ -3,11 +3,11 @@ n, k = map(int, input().split())
 # 동적계획법 사용
 coins = [int(sys.stdin.readline()) for _ in range(n)]
 dp = [0 for _ in range(k+1)]
-coins.sort()
+dp[0] = 1  # 동전이 1개일 경우
 #  10 --> 1, 2, 5
 # 모든 코인을 사용
 for coin in coins:
-
-
-print(cnt)
-
+    for i in range(coin, k+1):
+        if i-coin >= 0:
+            dp[i] += dp[i-coin]
+print(dp[k])
