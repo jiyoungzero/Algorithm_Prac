ec4870e888441eb7aef0f6f0c091f0964c7b774e
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-11 23:10:19 +0900

    need to retry & understand

--- boj_동전1.py
+++ boj_동전1.py
@@ -4,8 +4,7 @@ n, k = map(int, input().split())
 coins = [int(sys.stdin.readline()) for _ in range(n)]
 dp = [0 for _ in range(k+1)]
 dp[0] = 1  # 동전이 1개일 경우
-#  10 --> 1, 2, 5
-# 모든 코인을 사용
+
 for coin in coins:
     for i in range(coin, k+1):
         if i-coin >= 0:
