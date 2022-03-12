dc940cf6d77601140185fbf48b84bca1dd9e6ede
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-12 23:25:47 +0900

    succeed and need to retry 2

--- boj_동전1.py
+++ boj_동전1.py
@@ -5,8 +5,11 @@ coins = [int(sys.stdin.readline()) for _ in range(n)]
 dp = [0 for _ in range(k+1)]
 dp[0] = 1  # 동전이 1개일 경우
 
+#     1 2 3 4 5 6 7 8 9 10
+# 1 /  1 1 1 1 1 1 1 1 1  1
+# 2 / 0 1 1 2 2 3 3 4 4  5
+# 5 /  0 0 0 0 1 1 2 2 3  4
 for coin in coins:
     for i in range(coin, k+1):
-        if i-coin >= 0:
-            dp[i] += dp[i-coin]
+        dp[i] += dp[i-coin]
 print(dp[k])
