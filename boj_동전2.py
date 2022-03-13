8c7067ba436a99498eb19096ba67449c5da39a79
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-13 23:36:10 +0900

    retry 2

--- boj_동전2.py
+++ boj_동전2.py
@@ -1,15 +1,1 @@
-# 동전0,1 문제도 풀기(다이나믹프로그래밍)
-
-import sys
-x, n = map(int, input().split())
-coins = list(map(int ,sys.stdin.readline().split()))
-coins.sort(reverse=True)
-dp = [1000001]*(n+1) # 동전의 개수를 나타내는 배열
-dp[0] = 0
-
-for coin in coins:
-	for i in range(coin, n+1):
-		dp[i] = min(dp[i], dp[i-coin]+1)
-if dp[n] == 1000001:
-    	print(-1)
-else:print(dp[n])
+# (다이나믹프로그래밍)
