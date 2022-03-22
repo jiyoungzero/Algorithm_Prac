e54f9be9e56db2f0d813e1fbf035824e43a4f791
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-22 20:36:37 +0900

    graph for bfs

--- boj_2606바이러스.py
+++ boj_2606바이러스.py
@@ -1,2 +1,12 @@
 import sys
 from collections import deque
+
+n = int(input())
+pair = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(pair):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(graph)
