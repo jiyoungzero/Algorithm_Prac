d0363598b5f8a5f3b24a4db806ebbead1b7f9971
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-23 16:21:44 +0900

    try 1

--- boj_2606바이러스.py
+++ boj_2606바이러스.py
@@ -3,8 +3,23 @@ from collections import deque
 
 n = int(input())
 pair = int(input())
+ans = 0
 graph = [[] for _ in range(n+1)]
 for _ in range(pair):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
+
+
+def bfs(x):
+    global ans
+    queue = deque()
+    queue.append(x)
+    while queue:
+        x = queue.popleft()
+        queue.append(x)
+        ans += 1
+    return ans
+
+
+print(bfs(1))
