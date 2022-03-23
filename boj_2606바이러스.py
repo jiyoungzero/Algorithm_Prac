5e20725efa7f79544a2ddd7fae8763601b7ca11d
Jiyoung Lee <wldudwldhd@naver.com>
2022-03-23 16:28:43 +0900

    try 1_Add visited array

--- boj_2606바이러스.py
+++ boj_2606바이러스.py
@@ -5,6 +5,8 @@ n = int(input())
 pair = int(input())
 ans = 0
 graph = [[] for _ in range(n+1)]
+visited = [False] * n
+
 for _ in range(pair):
     a, b = map(int, input().split())
     graph[a].append(b)
