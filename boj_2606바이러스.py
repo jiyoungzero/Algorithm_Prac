891e67be8ec3ac7282811f97b01796db6b5f297a
이지영 <wldudwldhd@naver.com>
2022-03-24 15:51:36 +0900

    more easy way to understand dp algorithm

--- boj_2606바이러스.py
+++ boj_2606바이러스.py
@@ -5,7 +5,7 @@ n = int(input())
 pair = int(input())
 ans = 0
 graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
+visited = [False] * (n+1) ## 사람 수에 따라서 visited 함수를 봐야 해서 n+1!!!
 
 for _ in range(pair):
     a, b = map(int, input().split())
