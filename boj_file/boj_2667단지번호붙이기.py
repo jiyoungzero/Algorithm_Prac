63ff7f76b48a000a2c7a39cfec541fef035ff36c
이지영 <wldudwldhd@naver.com>
2022-03-28 14:40:05 +0900

    first try_doesn't print

--- boj_file/boj_2667단지번호붙이기.py
+++ boj_file/boj_2667단지번호붙이기.py
@@ -1,27 +1,30 @@
-import sys
 from collections import deque
 
-r = sys.stdin.readline()
-n = int(r)
-ans = 0
+n = int(input())
 dxs = [0, 0, 1, -1]
 dys = [1, -1, 0, 0]
-graph = [list(map(int, r)) for _ in range(n)]
-visited = [[0]*(n+1) for _ in range(n+1)]
+graph = [list(map(int, input())) for _ in range(n)]
+visited = [[False]*(n+1) for _ in range(n+1)]
+count = []
 
-def bfs(x,y):
-    global ans
+
+def bfs(x, y):
+    global count
     queue = deque([])
-    queue.append([x,y])
-    visited[x][y] = 1
+    queue.append([x, y])
+    visited[x][y] = True
     while queue:
-        x, y = queue.popleft([])
+        cnt = 0
+        x, y = queue.popleft()
         for i in range(4):
             nx, ny = x+dxs[i], y+dys[i]
-            if nx >= 0 and nx < n and ny >= 0 and ny < n:
-                queue.append([nx,ny])
-                visited[nx][ny] += 1
-    ans += 1 
+            if nx >= 0 and nx < n and ny >= 0 and ny < n and graph[nx][ny] == True:
+                queue.append([nx, ny])
+                visited[nx][ny] = True
+                cnt += 1
+    count.append[cnt]
+    return count
 
-bfs(0,0)
-print(ans)
+
+bfs(0, 0)
+print(len(count))
