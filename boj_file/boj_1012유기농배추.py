f07bdce80ccb253d3514c127d9ea6de2fc4fc336
이지영 <wldudwldhd@naver.com>
2022-04-01 00:38:29 +0900

    input

--- boj_file/boj_1012유기농배추.py
+++ boj_file/boj_1012유기농배추.py
@@ -1,3 +1,9 @@
 # bfs
 from collections import deque
 import sys
+
+n = int(input())
+w, h, k = map(int, input().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(k)]
+
+print(graph)
