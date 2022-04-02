5cbb67e646ceb9ca14249b0d47f2b25bd89d04b5
이지영 <wldudwldhd@naver.com>
2022-04-03 02:01:59 +0900

    add code : test case for loop, result list, switching w, h input

--- boj_file/boj_1012유기농배추.py
+++ boj_file/boj_1012유기농배추.py
@@ -1,38 +1,45 @@
 # bfs
 from collections import deque
-from glob import glob
 import sys
 
 n = int(input())
-w, h, k = map(int, input().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(k)]
-visited = [[False]*(w+1) for _ in range(h+1)]
-cnt = 0
 dxs = [0, 0, 1, -1]
 dys = [1, -1, 0, 0]
-count = []
 
 
-def bfs(x, y):
+def bfs(graph, x, y):
     global cnt
     queue = deque()
-    visited[x][y] = True
-    cnt = 1
+    queue.append((x, y))
+    graph[x][y] = 0
+
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            nx, ny = x+dxs[i], y+dys[I]
-            if nx < 0 or ny < 0 or nx >= h or ny >= w:
+            nx, ny = x+dxs[i], y+dys[i]
+            if nx < 0 or ny < 0 or nx >= w or ny >= h:
                 continue
-            if graph[nx][ny] == 1 and visited[nx][ny] == False:
+            if graph[nx][ny] == 1:
                 queue.append((nx, ny))
-                visited[nx][ny] = True
+                graph[nx][ny] = 0  # 다음 케이스를 위해 초기화를 미리 해주기
+    return
+
+
+result = []
+for _ in range(n):
+    w, h, k = map(int, input().split())
+    graph = [[0]*(h) for _ in range(w)]
+    cnt = 0
+
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[a][b] = 1
+
+    for i in range(w):
+        for j in range(h):
+            if graph[i][j] == 1:
+                bfs(graph, i, j)
                 cnt += 1
-    return cnt
-
-
-for i in range(h):
-    for j in range(w):
-        if not visited[i][j] and graph[i][j] == 1:
-            count.append(bfs(i, j))
-print(len(count))
+    result.append(cnt)
+for ele in result:
+    print(ele)
