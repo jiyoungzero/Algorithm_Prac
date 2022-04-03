e29200b34da4a98b0254cf3cc6c3159018816b1f
이지영 <wldudwldhd@naver.com>
2022-04-03 22:27:48 +0900

    index error

--- boj_file/boj_7576토마토.py
+++ boj_file/boj_7576토마토.py
@@ -1,11 +1,37 @@
 from collections import deque
 import sys
 
-m, n = int(input().split())
+# 입력 받을 때 아예 반대로 받기 > 나중에 x, y 좌표 편하게 하기 위해
+n, m = map(int,input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0]*(m+1) for _ in range(n+1)]
+dxs, dys = [0, 0, 1, -1], [1, -1, 0, 0]
+queue = deque([])
 
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            queue.append([i, j])
+
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx, ny = x+dxs[k], y+dys[k]
+            if nx < 0 or ny < 0 or nx >= n or ny > m:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] += 1
+                queue.append([nx, ny])
+
+
+ans = -10
+bfs()
+for row in graph:
+    for ele in row:
+        if ele == 0:
+            print(-1)
+            exit(0)
+        ans = max(ans, ele)
+print(ans)
