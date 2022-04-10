6c29c48005ebba8b03daed45fe14d7db2408b9f3
이지영 <wldudwldhd@naver.com>
2022-04-10 18:40:10 +0900

    programmers_완주하지 못한 선수_first commit

--- boj_file/programmers_완주하지못한선수.py
+++ boj_file/programmers_완주하지못한선수.py
@@ -1,3 +1,12 @@
-import sys
-
-n = int(input())
+def solution(participant, completion):
+    answer = ''
+    for par in participant:
+        for com in completion:
+            if par == com:
+                participant.remove(par)
+                completion.remove(par)
+                if par in participant:
+                    answer = par
+                    return answer
+    answer = participant[0]
+    return answer
