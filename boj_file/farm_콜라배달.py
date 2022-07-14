caee322249ab2b178ca9fac9b6462d62f60d8a2e
이지영 <wldudwldhd@naver.com>
2022-07-15 00:22:22 +0900

    succeed

--- boj_file/farm_콜라배달.py
+++ boj_file/farm_콜라배달.py
@@ -1,19 +1,17 @@
 # 완전탐색
 
 n = int(input())
-ans = 0
-max_num = n // 2
-
-if n % 5 == 0:
-	ans = n//5
-	print(ans)
-elif n % 5 != 0 and n % 3 == 0:
-	if n // 5 >= 3:
-		ans = n//5  + (n % 5)//3
-		print(ans)
-	else:
-		ans = n//3
-		print(ans)
-
+ans = []
+three = n // 3
+five = n//5
+min_ans = 0
+for i in range(three+1):
+	for j in range(five+1):
+		if (i*3 + j*5) == n:
+			ans.append(i+j)
+		else:
+			continue
+if len(ans) > 0:
+	print(min(ans))
 else:
 	print(-1)
