73918bfae134d6e748190984d8f348323fcafd6c
이지영 <wldudwldhd@naver.com>
2022-07-16 01:38:25 +0900

    succed:switch i, j

--- boj_file/farm_덩치계산.py
+++ boj_file/farm_덩치계산.py
@@ -2,15 +2,17 @@
 n = int(input())
 lst = [tuple(map(int, input().split())) for _ in range(n)]
 ans = [n for _ in range(n)]
+n = int(input())
+lst = [tuple(map(int, input().split())) for _ in range(n)]
+ans = []
+bigger = 0
 
 for i in range(n):
-	for j in range(i, n):
-		if lst[i][0] > lst[j][0] and lst[i][1] > lst[j][1]:
-			ans[i] -= 1
-		elif (lst[i][0] > lst[j][0] and lst[i][1] < lst[j][1]) or (lst[i][0] < lst[j][0] and lst[i][1] > lst[j][1]):
-			ans[i] -= 1 
-			ans[j] -= 1
-		elif lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
-			ans[j] -= 1
+	for j in range(n):
+		if i != j and lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
+			bigger += 1 
+	ans.append(bigger+1)
+	bigger = 0
 
-print(ans)
+for ele in ans:
+	print(ele, end=' ')
